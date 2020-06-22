6112dcce49efcc25479a60b6b9d154e27f7cddd0
Lucio Nonato <luciononato@hotmail.com>
2020-06-21 21:50:55 -0300

    9 commit
    pendente esconder e aparecer

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -2,10 +2,10 @@ const cards = document.querySelectorAll('.card')
 
 for(let card of cards) {
     card.addEventListener('click', function(){
-        const recipeTitle = card.getAttribute("name")
+        const recipeTitle = card.getAttribute('name')
         let valor = 0
         for(let card of cards){
-            if(recipeTitle == card.getAttribute("name")){
+            if(recipeTitle == card.getAttribute('name')){
                 window.location.href = `/recipes/${valor}`
             }
             valor ++
