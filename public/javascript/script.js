fa29a5f39846f299822d64188ef160fe0cbcb855
Lucio Nonato <luciononato@hotmail.com>
2020-07-02 22:53:46 -0300

    9.3 commit

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,18 +1,14 @@
 //Redirecionando para receita clicada
 const cards = document.querySelectorAll('.card')
 
-for(let card of cards) {
-    card.addEventListener('click', function(){
-        const recipeTitle = card.getAttribute('name')
+for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click', function () {
         let valor = 0
-        for(let card of cards){
-            if(recipeTitle == card.getAttribute('name')){
-                window.location.href = `/recipe/${valor}`
-            }
-            valor ++
-        }
+        valor += i
+        window.location.href = `/recipes/${valor}`
     })
 }
+
 
 //Selecionar active por página
 const currentPage = location.pathname
@@ -23,4 +19,19 @@ for (item of menuItems){
         item.classList.add("active")
     }
 }
+console.log(currentPage)
 
+// 
+const recipeInformations = document.querySelectorAll('.recipe-information')
+for(let information of recipeInformations) {
+    const hide = information.querySelector('.hide')
+
+    hide.addEventListener('click', function() {
+        information.querySelector('.information').classList.toggle('hidden')
+        if (hide.innerHTML == 'ESCONDER') {
+            hide.innerHTML = 'MOSTRAR'
+        } else {
+            hide.innerHTML = 'ESCONDER'
+        }
+    })
+}
--- routes.js
+++ routes.js
@@ -5,10 +5,10 @@ const main = require("./controller/main")
 
 
 //rotas
-routes.get("/", main.index)
-routes.get("/about", main.about)
-routes.get("/recipes", main.recipes)
-routes.get("/recipes/:index", main.recipe)
+routes.get('/', main.index)
+routes.get('/about', main.about)
+routes.get('/recipes', main.recipes)
+routes.get('/recipes/:index', main.recipe)
 
 
 module.exports = routes
