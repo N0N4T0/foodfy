21d2920cf0cabc39497b4f5d76557041d765fccb
Lucio Nonato <luciononato@hotmail.com>
2020-07-15 18:15:37 -0300

    10.7 commit
    funcionando por completo sem integrar ao banco de dados.

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -30,7 +30,6 @@ module.exports = {
     
         fs.writeFile('data.json', JSON.stringify(data, null, 2), function (err) {
             if (err) return res.send('Write file error!')
-            // return res.redirect('/admin/recipes')
             return res.redirect(`/admin/recipes/${id}`)
         })
     },
@@ -51,40 +50,63 @@ module.exports = {
         return res.render('admin/show', { recipe })
     },
     
-    
     edit(req, res) {
-        const recipeIndex = req.params.index
+        const { id } = req.params
+
+        const foundRecipe = data.recipes.find(function(recipe) {
+            return recipe.id == id
+        })
+    
+        if(!foundRecipe) return res.send('Receita not found!');
+
         const recipe = {
-            ...data.recipes[recipeIndex],
-            index: recipeIndex
+            ...foundRecipe,
         }
+
         return res.render('admin/edit', { recipe })
     },
     
     put(req, res) {
-        const recipeIndex = req.params.index
-        const keys = Object.keys(req.body)
-    
-        for (let key of keys) {
-            if (req.body[key] == '')
-                return res.send('Por favor, preencha todos os campos.')
+        const { id } = req.body;
+        let index
+
+        const foundRecipe = data.recipes.find(function(recipe, foundIndex){
+            if(recipe.id  == id){
+                index = foundIndex            
+                return true
+            }     
+        })
+
+        if(!foundRecipe) return res.send("Recipe not found")
+
+        const recipe = {
+            ...foundRecipe,
+            ...req.body,
+            id: Number(id),
+            birth: Date.parse(req.body.birth),
         }
-    
-        data.recipes[recipeIndex] = { ...req.body }
+
+        data.recipes[index] = recipe
     
         fs.writeFile('data.json', JSON.stringify(data, null, 2), function (err) {
             if (err) return res.send('Write file error!')
-            return res.redirect(`/admin/recipes/${recipeIndex}`)
+
+            return res.redirect(`/admin/recipes/${id}`)
         })
     },
     
     delete(req, res) {
-        const recipeIndex = req.params.index
+        const { id } = req.body
+
+        const filteredRecipes = data.recipes.filter(function(recipe){
+            return recipe.id != id
+        })
     
-        data.recipes.splice(recipeIndex, 1)
+        data.recipes = filteredRecipes
     
         fs.writeFile('data.json', JSON.stringify(data, null, 2), function (err){
             if (err) return res.send('Write file error!')
+            
             return res.redirect('/admin/recipes')
         })
     }
