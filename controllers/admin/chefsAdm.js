7d399e806ef5fc2618e1e1ccde5be1114f58191c
Lucio Nonato <luciononato@hotmail.com>
2020-07-18 15:37:29 -0300

    11.3 commit
    form posicionado errado bugava o post. A partir daqui começar integração com banco de dados.

--- controllers/admin/chefsAdm.js
+++ controllers/admin/chefsAdm.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const data = require("../../data.json")
+const { put } = require('./recipes')
 
 module.exports = {
     index(req, res) {
@@ -12,10 +13,10 @@ module.exports = {
 
     post(req, res) {
         const keys = Object.keys(req.body)
-
-        for (let key of keys) {
-            if(req.body[key] == '')
-                return res.send('Por favor, preencha todos os campos')
+    
+        for (key of keys) {
+            if (req.body[key] == '')
+                return res.send('Por favor, preencha todos os campos.')
         }
 
         let { name, image } = req.body
@@ -23,7 +24,8 @@ module.exports = {
 
         data.chefs.push({
             id,
-            ...req.body
+            name,
+            image,
         })
 
         fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) {
@@ -48,5 +50,63 @@ module.exports = {
         return res.render('admin/chefs/show', { chef })
     },
 
+    edit(req, res) {
+        const { id } = req.params
+
+        const foundChef = data.chefs.find(function(chef) {
+            return chef.id == id
+        })
+
+        if (!foundChef) return res.send('Chef não encontrado!')
+
+        const chef = {
+            ...foundChef,
+        }
+
+        return res.render('admin/chefs/edit', { chef })
+    },
+
+    put(req, res) {
+        const { id } = req.body
+        let index
+
+        const foundChef = data.chefs.find(function(chef, foundIndex){
+            if(chef.id == id) {
+                index = foundIndex
+                return true
+            }
+        })
+
+        if(!foundChef) return res.send("Chef não encontrado")
+
+        const chef = {
+            ...foundChef,
+            ...req.body,
+            id: Number(id)
+        }
+
+        data.chefs[index] = chef
+
+        fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) {
+            if (err) return res.send('Write file error') 
+            return res.redirect(`/admin/chefs/${id}`)
+        })
+    },
+
+    delete(req, res) {
+        const { id } = req.body
+
+        const filteredChefs = data.chefs.filter(function(chef){
+            return chef.id != id
+        })
+
+        data.chefs = filteredChefs
+
+        fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) {
+            if (err) return res.send('Write file error') 
+            
+            return res.redirect('/admin/chefs')
+        })
+    }
 
 }
--- routes.js
+++ routes.js
@@ -27,7 +27,10 @@ routes.delete("/admin/recipes", recipes.delete) // Deletar uma receita
 routes.get("/admin/chefs", chefsAdmn.index)
 routes.get("/admin/chefs/create", chefsAdmn.create)
 routes.get("/admin/chefs/:id", chefsAdmn.show)
+routes.get("/admin/chefs/:id/edit", chefsAdmn.edit)
 routes.post("/admin/chefs", chefsAdmn.post)
+routes.put("/admin/chefs", chefsAdmn.put)
+routes.delete("/admin/chefs", chefsAdmn.delete)
 
 
 module.exports = routes
