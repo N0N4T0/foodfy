0883373a8849f454b13e82aec3bb3cf7875599a6
Lucio Nonato <luciononato@hotmail.com>
2020-08-31 17:51:31 -0300

    13.2 commit
    adcionado miniatura das fotos ao fazer upload.

---
+++ database.sql
@@ -0,0 +1,44 @@
+DROP DATABASE IF EXISTS foodfy;
+CREATE DATABASE foodfy;
+
+CREATE TABLE files (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    path TEXT NOT NULL
+);
+
+CREATE TABLE chefs (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    file_id INTEGER REFERENCES files(id),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE recipes (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    ingredients TEXT[] NOT NULL,
+    preparations TEXT[] NOT NULL,
+    information TEXT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW(),
+
+    chef_id INTEGER
+    REFERENCES chefs(id)
+);
+
+CREATE TABLE recipe_files (
+    id SERIAL PRIMARY KEY,
+
+    recipe_id INTEGER NOT NULL
+    REFERENCES recipes(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+
+
+    file_id INTEGER NOT NULL
+    REFERENCES files(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
